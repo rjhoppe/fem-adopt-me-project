885a4d3e5dbee2e40f32d4fb775d7bb045dc29ee
rjhoppe-W <48058894+rjhoppe@users.noreply.github.com>
2024-04-01 12:21:39 -0400

    feat: Implemented fetch method to use with React Query

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,32 @@
 import { createRoot } from "react-dom/client";
 import { Link, BrowserRouter, Route, Routes } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import SearchParams from "./SearchParams";
 import Details from "./Details";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      // Can change TTL of cache here, it is in milliseconds
+      // 1000 * 60 * 10 => 10 minutes
+      staleTime: Infinity,
+      cacheTime: Infinity,
+    },
+  },
+});
 
 const App = () => {
   return (
     <BrowserRouter>
-      <header>
-        <Link to="/">Adopt Me</Link>
-      </header>
-      <Routes>
-        <Route path="/details/:id" element={<Details />} />
-        <Route path="/" element={<SearchParams />} />
-      </Routes>
+      <QueryClientProvider client={queryClient}>
+        <header>
+          <Link to="/">Adopt Me</Link>
+        </header>
+        <Routes>
+          <Route path="/details/:id" element={<Details />} />
+          <Route path="/" element={<SearchParams />} />
+        </Routes>
+      </QueryClientProvider>
     </BrowserRouter>
   );
 };
