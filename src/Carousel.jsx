55bfd49e493a1c692c9ef2a806d2e7bf0d854368
rjhoppe-W <48058894+rjhoppe@users.noreply.github.com>
2024-04-02 13:47:09 -0400

    feat: Continued building Carousel class comp

--- src/Carousel.jsx
+++ src/Carousel.jsx
@@ -9,6 +9,15 @@ class Carousel extends Component {
     images: ["http://pets-v2.dev-apis.com/pets/none.jpg"],
   };
 
+  // arrow function will capture the scope of wherever it is written
+  // therefore, capturing what component .this is referring to
+  handleIndexClick = (e) => {
+    this.setState({
+      // "+" in front turns string into a number
+      active: +e.target.dataset.index,
+    });
+  };
+
   render() {
     const { active } = this.state;
     const { images } = this.props;
@@ -18,7 +27,10 @@ class Carousel extends Component {
         <img src={images[active]} alt="animal hero" />
         <div className="carousel-smaller">
           {images.map((photo, index) => (
+            // eslint-disable-next-line
             <img
+              onClick={this.handleIndexClick}
+              data-index={index}
               key={photo}
               src={photo}
               className={index === active ? "active" : ""}
